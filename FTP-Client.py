f637fd4479f4299bff2757b0de3cb6a58b63137e
AgogFox <bopawat@gmail.com>
2024-03-31 02:21:33 +0700

    refactor

--- FTP-Client.py
+++ FTP-Client.py
@@ -6,13 +6,13 @@ from getpass import getpass
 cmd_sock = None
 host = None
 
-not_need_connection = [
+NOT_NEED_CONNECTION = (
     "bye",
     "open",
     "quit"
-]
+)
 
-need_connection = [
+NEED_CONNECTION = (
     "ascii",
     "binary",
     "cd",
@@ -25,7 +25,7 @@ need_connection = [
     "pwd",
     "rename",
     "user"
-]
+)
 
 
 def ftp_send_cmd(socket: socket, str: str): #format string and send to ftp server
@@ -33,17 +33,15 @@ def ftp_send_cmd(socket: socket, str: str): #format string and send to ftp serve
     return
 
 def get_resp(socket: socket) -> str:
-    return socket.recv(1024).decode()
+    return socket.recv(1024).decode().strip()
 
 def close_cmd_sock():
     global cmd_sock
     try:
         cmd_sock.close()
         cmd_sock = None
-        return
     except:
         cmd_sock = None
-        return
 
 def ftp_open_data_conn() -> socket:
     global host
@@ -51,34 +49,37 @@ def ftp_open_data_conn() -> socket:
     data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     data_sock.bind((host, 0))
     data_sock.listen()
-    port = data_sock.getsockname()[1] #get port number
-
     host_part = host.replace('.', ',')
+
+    port = data_sock.getsockname()[1] #get port number
     port = f"{port:016b}" #convert port number to bianry
     port_1 = int(port[0:8], 2) #first part of the port
     port_2 = int(port[8:16], 2) #second part of the port
+    
     ftp_send_cmd(cmd_sock, f"PORT {host_part},{port_1},{port_2}")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
     #TODO: add exception if fail
     return data_sock
 
-def measure(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
+def measure(text: str):
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
 
-        result = func(*args, **kwargs)
+            result = func(*args, **kwargs)
 
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        if elapsed_time == 0: #prevent very fast transfer(start_time = end_time) from causeing devide by 0
-            elapsed_time = 0.0001
-        
-        speed = result[0] / (elapsed_time * 1000) #result[0] = size of data
-        status = f"ftp: {result[0]} bytes received in {elapsed_time:.3f}Seconds {speed:.2f}Kbytes/sec."
-        return status, result
-    return wrapper
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            if elapsed_time == 0: #prevent very fast transfer(start_time = end_time) from causeing devide by 0
+                elapsed_time = 0.0001
+            
+            speed = result[0] / (elapsed_time * 1000) #result[0] = size of data
+            status = f"ftp: {result[0]} bytes {text} in {elapsed_time:.3f}Seconds {speed:.2f}Kbytes/sec."
+            return status, result
+        return wrapper
+    return inner
 
-@measure
+@measure("received")
 def recv_data(data_sock: socket):
     data_conn, data_addr = data_sock.accept()
     size = 0
@@ -87,12 +88,12 @@ def recv_data(data_sock: socket):
         data_part = data_conn.recv(1024)
         if data_part:
             data += data_part.decode()
-            size += len(data_part)
         else:
             break
+    size = len(data)
     return size + 3, data #Plus 3 because of IDK
 
-@measure
+@measure("sent")
 def send_data(data_sock, file):
     data_conn, data_addr = data_sock.accept()
     size = 0
@@ -110,18 +111,18 @@ def send_data(data_sock, file):
 
 def ascii():
     ftp_send_cmd(cmd_sock, "TYPE A")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
     return
 
 def binary():
     ftp_send_cmd(cmd_sock, "TYPE I")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
     return
 
 def bye():
     try:
         ftp_send_cmd(cmd_sock, "QUIT")
-        print(get_resp(cmd_sock), end="")
+        print(get_resp(cmd_sock))
         close_cmd_sock()
     except:
         pass
@@ -129,7 +130,7 @@ def bye():
 
 def cd(remote_dir: str, *argv):
     ftp_send_cmd(cmd_sock, f"CWD {remote_dir}")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
     return
 
 def close():
@@ -137,61 +138,50 @@ def close():
         ftp_send_cmd(cmd_sock, "QUIT")
     except:
         print("Not connected.")
-        return;
-
-    print(get_resp(cmd_sock), end="")
-    close_cmd_sock()
-    return
-
-def delete(remote_fname: str = "", *argv):
-    if not remote_fname:
-        remote_fname = input("Remote file ")
-    
-    if not remote_fname:
-        print("delete remote file.")
         return
 
-    ftp_send_cmd(cmd_sock, f"DELE {remote_fname}")
-    print(get_resp(cmd_sock), end="")
-    return
+    print(get_resp(cmd_sock))
+    close_cmd_sock()
 
-def get(remote_fname: str = "", local_fname: str = "", *argv):
-    if not remote_fname:
-        remote_fname = input("Remote file ")
-        if not remote_fname:
+def delete(remote_file: str = "", *argv):
+    if not remote_file:
+        remote_file = input("Remote file ")
+        if not remote_file:
+            print("delete remote file.")
+            return
+    
+    ftp_send_cmd(cmd_sock, f"DELE {remote_file}")
+    print(get_resp(cmd_sock))
+
+def get(remote_file: str = "", local_file: str = "", *argv):
+    if not remote_file:
+        remote_file = input("Remote file ")
+        if not remote_file:
             print("Remote file get [ local-file ].")
             return
         
-        local_fname = input("Local file ")
-        if not local_fname:
-            local_fname = remote_fname
-    else:
-        if not local_fname:
-            local_fname = remote_fname
+        local_file = input("Local file ")
+
+    if not local_file:
+        local_file = remote_file
 
     data_sock = ftp_open_data_conn()
-    ftp_send_cmd(cmd_sock, f"RETR {remote_fname}")
-    resp = get_resp(cmd_sock)
-    resp_phrase = resp.split()[1]
+    ftp_send_cmd(cmd_sock, f"RETR {remote_file}")
+    resp = get_resp(cmd_sock) #Note: double \r\n in "550 This function is not supported on this system.\r\n\r\n"
+    print(resp)
 
-    #hack for dealing with \r\n\r\n in "This function is not supported on this system.\r\n\r\n"
-    if resp_phrase == "This":
-        print(resp[:-2], end="") #[:-2] because there're two \r\n in respond
-        return
-    elif resp_phrase == "Permission":
-        print(resp, end="")
+    if resp.startswith("550"):
         return
 
     status, result = recv_data(data_sock)
-    file = open(local_fname, 'w')
+    file = open(local_file, 'w')
     file.write(result[1])
     file.close()
-    print(get_resp(cmd_sock), end="")
-    if result[0] == 3:
+    print(get_resp(cmd_sock))
+    if result[0] == 3: #if file is empty
         return
-    else:
-        print(status)
-        return
+    print(status)
+
 
 def ls(remote_dir: str = "", *argv) -> None:
     data_sock = ftp_open_data_conn()
@@ -209,7 +199,7 @@ def ls(remote_dir: str = "", *argv) -> None:
 
     status, result = recv_data(data_sock)
     print(result[1], end="")
-    print(get_resp(cmd_sock), end="") #226 Operation successful
+    print(get_resp(cmd_sock)) #226 Operation successful
     #TODO: add exception
     print(status)
     return
@@ -229,15 +219,14 @@ def ftp_open(host_local: str = None, port: str = "21", *argv):
     cmd_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if not host_local:
         host_local = input("To ")
-    
-    if not host_local:
-        print("Usage: open host name [port]")
-        return
+        if not host_local:
+            print("Usage: open host name [port]")
+            return
     
     #set recieved host from funciton to global var for future use
     host = host_local 
 
-    #attemp establish connection
+    #attempt establish connection
     try:
         cmd_sock.connect((host_local, int(port)))
     except ConnectionRefusedError:
@@ -256,90 +245,86 @@ def ftp_open(host_local: str = None, port: str = "21", *argv):
         print(type(e).__name__, e.args)
         return
 
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
 
     #login
     user = input(f"User ({host_local}:(none)): ")
     ftp_send_cmd(cmd_sock, f"USER {user}")
 
-    resp = get_resp(cmd_sock )#respond for username input
-    print(resp, end='')
+    resp = get_resp(cmd_sock)#respond for username input
     resp_code = resp.split()[0]
+    print(resp)
 
     if resp_code == "501": #missing user argument
         print("Login failed.")
-        return
-    elif resp_code == "331":
+    
+    elif resp_code == "331": #get password
         password = getpass("Password: ")
-        print("")
+        print()
         ftp_send_cmd(cmd_sock, f"PASS {password}")
         resp = get_resp(cmd_sock)
         resp_code = resp.split()[0]
+
         if resp_code == "530": #530 Login incorrect.
-            print(resp, end="")
+            print(resp)
             print("Login failed")
-            return
         elif resp_code == "230": #Login successful
-            print(resp, end="")
-            return
+            print(resp)
         else:
             print("unexpected error, password")
-            return
+    
     else:
         print("unexpected error, user")
-        return
 
-def put(local_fname: str = "", remote_fname: str = "", *argv):
-    if not local_fname:
-        local_fname = input("Local file ")
-        if not local_fname:
+def put(local_file: str = "", remote_file: str = "", *argv):
+    if not local_file:
+        local_file = input("Local file ")
+        if not local_file:
             print("Local file put: remote file.")
             return
         
-        remote_fname = input("Remote file ")
-        if not remote_fname:
-            remote_fname = local_fname
-    else:
-        if not remote_fname:
-            remote_fname = local_fname
+        remote_file = input("Remote file ")
+
+    if not remote_file:
+        remote_file = local_file
     
     try:
-        file = open(local_fname, "rb")
+        file = open(local_file, "rb")
     except FileNotFoundError:
-        print(f"{local_fname}: File not found")
+        print(f"{local_file}: File not found")
         return
 
     data_sock = ftp_open_data_conn()
-    ftp_send_cmd(cmd_sock, f"STOR {remote_fname}")
-    print(get_resp(cmd_sock), end="")
+    ftp_send_cmd(cmd_sock, f"STOR {remote_file}")
+    print(get_resp(cmd_sock))
     status, result = send_data(data_sock, file)
-    file.close()
-    data_sock.close()
-    print(get_resp(cmd_sock), end="")
-    if result[0] == 3:
+
+    file.close() #TODO: user 'with' statement
+    data_sock.close() #TODO: user 'with' statement
+
+    print(get_resp(cmd_sock))
+    if result[0] == 3: #if file is empty
         return
-    else:
-        print(status)
-        return
+    print(status)
 
 def pwd():
     ftp_send_cmd(cmd_sock, f"XPWD")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
     return
 
 def rename(from_name: str = "", to_name: str = "", *argv):
     if not from_name: #no argument specify
-        from_name = input("From name")
-        to_name = input("To name")
-    elif not to_name: #only one argument specify
-        to_name = input("To name")
+        from_name = input("From name ")
+    
+    if not to_name: #only one argument specify
+        to_name = input("To name ")
 
     ftp_send_cmd(cmd_sock, f"RNFR {from_name}")
     resp = get_resp(cmd_sock)
-    if resp.split()[0] == "350": #directory exists
-        print(resp, end="")
+    if resp.startswith("350"): #directory exists
+        print(resp)
         ftp_send_cmd(cmd_sock, f"RNTO {to_name}")
-        print(get_resp(cmd_sock), end="")
+        print(get_resp(cmd_sock))
     else:
         print("Unexpected error, rename command")
     return
@@ -352,10 +337,10 @@ def user(user: str = "", password: str ="", *argv):
             return
 
     ftp_send_cmd(cmd_sock, f"USER {user}")
-    print(get_resp(cmd_sock), end="")
+    print(get_resp(cmd_sock))
 
     if not password:
-        password = input("Password: ")
+        password = getpass("Password: ")
     
     ftp_send_cmd(cmd_sock, f"PASS {password}")
 
@@ -363,15 +348,12 @@ def user(user: str = "", password: str ="", *argv):
     resp_code = resp.split()[0]
 
     if resp_code == "530": #530 Login incorrect.
-        print(resp, end="")
+        print(resp)
         print("Login failed")
-        return
     elif resp_code == "230": #Login successful
-        print(resp, end="")
-        return
+        print(resp)
     else:
         print("unexpected error, password")
-        return
 
 
 
@@ -385,12 +367,9 @@ while True:
     
     command = input_list[0]
 
-    if len(input_list) > 1:
-        arg = input_list[1:]
-    else:
-        arg = ""
+    arg = input_list[1:]
     
-    if command in not_need_connection:
+    if command in NOT_NEED_CONNECTION:
         match command:
             case "bye":
                 bye()
@@ -401,7 +380,7 @@ while True:
             case "quit":
                 bye()
 
-    elif command in need_connection:
+    elif command in NEED_CONNECTION:
         if not cmd_sock:
             print("Not connected.")
             continue
